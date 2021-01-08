1df59b609cfe375933bcdbb845bb848fe12601b9
igorbrasileiro <brasileiro456@gmail.com>
2021-01-08 13:45:35 -0300

    Add bannerImageContainer to enhance imagem layout

--- react/components/Banner.tsx
+++ react/components/Banner.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { Image } from 'vtex.store-image'
 import type { ImageProps } from 'vtex.store-image/react/Image'
 
@@ -14,19 +14,39 @@ export interface Props {
   url: string
 }
 
-function Banner({ image, url, subtitle, title }: Props) {
+function getBannerColorClass(color: Color) {
+  switch (color) {
+    case 'blue':
+      return styles.bannerBlue
+
+    case 'red':
+      return styles.bannerRed
+
+    case 'yellow':
+      return styles.bannerYellow
+
+    default:
+      return ''
+  }
+}
+
+function Banner({ color, image, url, subtitle, title }: Props) {
   const desktopButtonClasses = 'pt4-m pr6-m pb4-m pl6-m'
   const mobileButtonClasses = 'pt3-s pr4-s pb3-s pl4-s'
+  const bannerClassColor = useMemo(() => getBannerColorClass(color), [color])
 
   return (
-    <>
+    <div
+      className={`w-100 flex justify-between ${bannerClassColor} ${styles.bannerContainer}`}
+    >
       <div
-        className={`absolute flex flex-column h-100 w-100 ${styles.bannerInfoContainer}`}
+        className={`flex flex-column h-100 z-1 ${styles.bannerInfoContainer}`}
       >
         <span className={`fw5 self-start ${styles.bannerTitle}`}>{title}</span>
         <span className={`fw5 self-start mb3 ${styles.bannerSubtitle}`}>
           {subtitle}
         </span>
+
         {url && (
           <div className="self-start">
             <a
@@ -38,8 +58,11 @@ function Banner({ image, url, subtitle, title }: Props) {
           </div>
         )}
       </div>
-      <Image {...image} width="100%" height="328px" />
-    </>
+
+      <div className={`absolute-s static-m ${styles.bannerImageContainer}`}>
+        <Image {...image} maxWidth="none" />
+      </div>
+    </div>
   )
 }
 
